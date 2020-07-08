654a5b5167f509f31f96d57db1e1f863de9b0bb4
kennja05 <jacobkenny05@gmail.com>
2020-07-08 09:32:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function findMinAndRemove(array){
-
+    let index = array.indexOf(Math.min(...array))
+    return array.splice(index, 1)
 }
 
 function insertionSort(array){
-
+    let sorted = []
+    while (array.length > 0){
+        sorted.push(parseInt(findMinAndRemove(array).join('')))
+    }
+    return sorted
 }
